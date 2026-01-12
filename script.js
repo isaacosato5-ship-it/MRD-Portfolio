df221acd1d4fc0fd8fd3604b77588a42485db272
Osato isaac <isaacosato5@gmail.cm>
2026-01-11 16:00:34 -1200

    made new changes

--- script.js
+++ script.js
@@ -26,45 +26,44 @@ const projects = [
     descriptons:
       "A comprehensive dashboard for e-commerce buisnesses with analytics, inventory management, and order tracking features.",
     techstack: ["React", "Chart.js", "Node.js", "MongoDB"],
-    Image: "./images/WhatsApp Image 2025-11-19 at 11.00.45 AM.jpeg",
+    images: "./images/1.jpeg",
   },
   {
     title: "Weather Forecast App",
     descriptons:
-      "A responsive weather application that provides accurate forecasts using data from a weather API with interactive maps",
+      "A responsive weather application that provides accurate forecasts using data from a weather API with interactive maps.",
     techstack: ["javascript", "API integration", "CSS Grid", "Local Storage"],
-    Image: "./images/WhatsApp Image 2026-01-11 at 10.42.10 PM.jpeg"
+    images: "./images/2.jpeg"
   },
    {
     title: "Task Management App",
     descriptons:
-      "A productivity tool that helps users organize tasks, set deadlines, and track progress with drag-drop functionality",
+      "A productivity tool that helps users organize tasks, set deadlines, and track progress with drag-drop functionality.",
       techstack: ["React", "Redux", "Firebase", "Material UI"],
-      images: "./images/WhatsApp Image 2026-01-11 at 10.42.16 PM.jpeg"
+      images: "./images/3.jpeg"
   },
 ];
-projects.map(function (projects, index) {
-  console.log(projects)
+const allprojects = projects.map(function (projects, index) {
   return(
-    ` <div class="projects-cards-parent">
+    ` <div class="projects-cards-parent" id=${index}>
            <div class="projectparents">
           <img
-            src="images/WhatsApp Image 2026-01-11 at 10.41.59 PM.jpeg "
-            class="for-image"
+            src= "${projects.images}" class ="for-image"
           />
           <br />
-          <span class="for-ecom">E-Commerce Dashboard</span>
+          <h1 class = "projectTitle">${projects.title}</h1>
           <p class="text-p">
-            A comprehensive dashboard for e-commerce buisnesses with analytics,
-            inventory management, and order tracking features.
+            ${projects.descriptons}
           </p>
           <div class="parent">
-            <div class="skills">React</div>
-            <div class="skills">Chart.js</div>
-            <div class="skills">Node.js</div>
-            <div class="skills">MongoDB</div>
+            ${projects.techstack.map(function (tech) {
+              return `
+              <div class="skills">${tech}</div> `
+            }).join("")}
           </div>
+          <button class="for-view">View Project</button>
         </div>
         </div>`
   )
-})
+}).join("")
+projectparent.innerHTML = allprojects
